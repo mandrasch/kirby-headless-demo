34e10d7d34bd0014e89fe6f7cc2eca94e986021a
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-04-23 15:51:15 +0200

    enable panel on local development only

---
+++ site/config/config.kirby-headless-demo.ddev.site.php
@@ -0,0 +1,9 @@
+<?php
+// https://getkirby.com/docs/guide/configuration#multi-environment-setup
+// Enable panel on local env - what's the best way for real world projects?
+
+return [
+    'panel' => [
+        'install' => true
+    ]
+];
