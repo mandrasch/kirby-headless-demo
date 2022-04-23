b84b8c0e2bd4a47df940acbf0e07d6de544b3dc7
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-04-23 17:20:42 +0200

    set config manually by now

--- astro-frontend/astro.config.mjs
+++ astro-frontend/astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
+// TODO: doesn't work to set it via gh action?!
+/* export default defineConfig({
     site: import.meta.env.site || 'https://localhost',
     base: import.meta.env.base || ''
+});*/
+
+// for gh pages deployment
+export default defineConfig({
+    site: 'https://mandrasch.github.io',
+    base: '/kirby-headless-demo'
 });
